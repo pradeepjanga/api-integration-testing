40e9b6894d9068bba552ae59a5e678d4f4b89592
pradeep janga <janga.pradeep@mayo.edu>
2023-03-08 13:24:05 -0600

    Add Other HTTP Request Operations for Testing

--- src/main/java/com/poc/apitesting/DemoIntegrationTest.java
+++ src/main/java/com/poc/apitesting/DemoIntegrationTest.java
@@ -8,13 +8,52 @@ import static io.restassured.RestAssured.given;
 public class DemoIntegrationTest {
 
 
-    public static void shouldGetDetailsAboutXXX() {
+    public static void shouldCallGetXXX() {
          Response response = given()
                 .contentType(ContentType.JSON)
-                .get("https://jsonplaceholder.typicode.com/todos/{id}", 1);
+                .get("https://jsonplaceholder.typicode.com/posts/{id}", 1);
          String output = response.asString();
          System.out.println(output);
+    }
 
+    public static void shouldCallPOSTXXX() {
+
+        String jsonPostBody = "{" +
+                "\"title\": \"foo\"," +
+                " \"body\": \"bar\"," +
+                " \"userId\": 1" +
+                " }";
+
+        Response response = given()
+                .contentType("application/json").body(jsonPostBody)
+                .when()
+                .post("https://jsonplaceholder.typicode.com/posts");
+        String output = response.asString();
+        System.out.println(output);
+    }
+
+    public static void shouldCallPUTXXX() {
+        String jsonPostBody = "{" +
+                "\"id\": 12,"+
+                "\"title\": \"foo\"," +
+                " \"body\": \"bar\"," +
+                " \"userId\": 1" +
+                " }";
+
+        Response response = given()
+                .contentType("application/json").body(jsonPostBody)
+                .when()
+                .put("https://jsonplaceholder.typicode.com/posts/{Id}", 1);
+        String output = response.asString();
+        System.out.println(output);
+    }
+
+    public static void shouldCallDELETEXXX() {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .delete("https://jsonplaceholder.typicode.com/posts/{id}", 1);
+        String output = response.asString();
+        System.out.println(output);
     }
 
 }
--- src/main/java/com/poc/apitesting/TriggerApi.java
+++ src/main/java/com/poc/apitesting/TriggerApi.java
@@ -3,7 +3,11 @@ package com.poc.apitesting;
 public class TriggerApi {
 
     public static void main(String[] args) {
-        System.out.println("test");
-        DemoIntegrationTest.shouldGetDetailsAboutXXX();
+        System.out.println("===============Automated Testing Started");
+        DemoIntegrationTest.shouldCallGetXXX();
+        DemoIntegrationTest.shouldCallPOSTXXX();
+        DemoIntegrationTest.shouldCallPUTXXX();
+        DemoIntegrationTest.shouldCallDELETEXXX();
+        System.out.println("===============Automated Testing Finished");
     }
 }
